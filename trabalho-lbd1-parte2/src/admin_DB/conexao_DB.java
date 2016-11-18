01d70ea0beb76d7cb657206565bf08a018b565df
THIAGO CARREIRA A. NASCIMENTO <16180161@l3043429.portoalegre.pucrsnet.br>
2016-11-18 20:36:06 -0200

    cria e fecha conexao

--- trabalho-lbd1-parte2/src/admin_DB/conexao_DB.java
+++ trabalho-lbd1-parte2/src/admin_DB/conexao_DB.java
@@ -1,27 +1,55 @@
-package admin_DB;
+//package admin_DB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class conexao_DB {
+    
+    private Connection conexao;
 
-	public void ConectarBanco() {
+	public void conectarBanco() {
 				
 		
 		Properties connectionProps = new Properties();
 		connectionProps.put("user", "de123456");
 		connectionProps.put("password", "de123456");
-		
-	
-		
-		//Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@//camburi.pucrs.br:1521/facin11g",connectionProps);
-	
-		
-		
-		
+		try{
+                  conexao = DriverManager.getConnection("jdbc:oracle:thin:@//camburi.pucrs.br:1521/facin11g",connectionProps);  
+                  System.out.println("Conexão bem sucedida!");
+                  //return conexao;
+                } catch(SQLException e){
+                    System.out.println("Falha na conexão!");                    
+                }
 	}
+        
+        public Connection getConexao(){
+            return this.conexao;
+        }
+        
+        public void fechaConexao(){
+            try {
+                this.conexao.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(conexao_DB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 	
+//        public void cadastraItem(String s){
+//        try {
+//            PreparedStatement stmt = this.conexao.prepareStatement(s);
+//            stmt.execute();
+//            stmt.close();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(conexao_DB.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        }
+        
+        
 	
 }
+
